4eddc6fe0bfac4af5acba5e052759a7c71cb11d3
Florian Varrin <fvarrin@student.42lausanne.ch>
2021-10-28 11:28:54 +0200

    Function strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fvarrin <florian.varrin@gmail.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/28 11:07:45 by fvarrin           #+#    #+#             */
+/*   Updated: 2021/10/28 11:28:30 by fvarrin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+#include <string.h>
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	if (haystack[0] == '\0' && needle[0] == '\0')
+		return ((char *)&haystack[0]);
+	i = 0;
+	while (haystack[i] && i < len)
+	{
+		j = 0;
+		if (haystack[i] == needle[j])
+		{
+			while (haystack[i + j] == needle[j] && needle[j])
+				j++;
+			if ((needle[j] == '\0') && len >= (i + j))
+				return ((char *)&haystack[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
