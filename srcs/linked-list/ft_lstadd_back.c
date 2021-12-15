1106e894a58258293a8af18621e67e3ff2ffdd1b
fvarrin <florian.varrin@gmail.com>
2021-12-15 11:09:40 +0100

    Fix first linked list functions

--- srcs/linked-list/ft_lstadd_back.c
+++ srcs/linked-list/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: fvarrin <florian.varrin@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 13:18:38 by fvarrin           #+#    #+#             */
-/*   Updated: 2021/12/15 11:06:52 by fvarrin          ###   ########.fr       */
+/*   Updated: 2021/12/15 11:09:35 by fvarrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,20 +26,3 @@ void	ft_lstadd_back(t_list **alst, t_list *new)
 	tmp = ft_lstlast(*alst);
 	tmp->next = new;
 }
-
-/* void	ft_lstadd_back(t_list **alst, t_list *new) */
-/* { */
-/* 	t_list	*tmp; */
-
-/* 	if (alst) */
-/* 	{ */
-/* 		if (*alst == NULL) */
-/* 			*alst = new; */
-/* 		else */
-/* 		{ */
-/* 			tmp = ft_lstlast(*(alst)); */
-/* 			ft_printf("tmp->content %s\n", tmp->content); */
-/* 			tmp->next = new; */
-/* 		} */
-/* 	} */
-/* } */
