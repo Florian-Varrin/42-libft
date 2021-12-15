760e569b792c1a9a0ae3e3cb11e8cb44ba71c814
fvarrin <florian.varrin@gmail.com>
2021-12-15 11:55:32 +0100

    Fix norminette

--- srcs/linked-list/ft_lstclear.c
+++ srcs/linked-list/ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: fvarrin <florian.varrin@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 11:19:31 by fvarrin           #+#    #+#             */
-/*   Updated: 2021/12/15 11:29:14 by fvarrin          ###   ########.fr       */
+/*   Updated: 2021/12/15 11:55:20 by fvarrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 	t_list	*next_el;
 
 	current_el = *lst;
-	while (current_el) {
+	while (current_el)
+	{
 		next_el = current_el->next;
 		ft_lstdelone(current_el, del);
 		current_el = next_el;
