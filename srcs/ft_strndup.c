193b4e97cd7d637e966eae8eacedb5a422aa3d6e
Florian Varrin <fvarrin@student.42lausanne.ch>
2022-06-03 12:53:26 +0200

    Improve stdndup

--- srcs/ft_strndup.c
+++ srcs/ft_strndup.c
@@ -6,24 +6,27 @@
 /*   By: fvarrin <florian.varrin@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 11:58:15 by fvarrin           #+#    #+#             */
-/*   Updated: 2021/11/05 16:27:47 by fvarrin          ###   ########.fr       */
+/*   Updated: 2021/10/29 16:34:40 by fvarrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stddef.h>
 #include <stdlib.h>
 
 char	*ft_strndup(const char *s1, size_t n)
 {
-	size_t	i;
+	int		i;
+	size_t	len;
 	char	*cpy;
 
-	cpy = (char *)malloc(sizeof(char) * (n + 1));
+	len = ft_strlen((char *)s1);
+	if (len > n)
+		len = n;
+	cpy = (char *)malloc(sizeof(char) * (len + 1));
 	if (!cpy)
 		return (NULL);
 	i = 0;
-	while (i < n)
+	while (i < len)
 	{
 		cpy[i] = s1[i];
 		i++;
